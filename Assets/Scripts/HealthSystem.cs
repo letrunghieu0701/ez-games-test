5332b616912d4207fe2b67434dacea4929714edf
Lê Trung Hiếu <hieuletrung0701@gmail.com>
2025-05-21 00:24:20 +0700

    Add simple health bar reduced animation

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -13,6 +13,9 @@ public class HealthSystem : MonoBehaviour
     private Image m_healthBarSpriteForeground;
     [SerializeField]
     private Transform m_healthBarTrs;
+    [SerializeField]
+    private float m_reduceHealhAnimationSpeed = 2f;
+    private float m_newFillAmount = 1;
 
     private Camera m_camera;
 
@@ -25,6 +28,7 @@ public class HealthSystem : MonoBehaviour
     private void Update()
     {
         m_healthBarTrs.rotation = Quaternion.LookRotation(m_healthBarTrs.position - m_camera.transform.position);
+        m_healthBarSpriteForeground.fillAmount = Mathf.MoveTowards(m_healthBarSpriteForeground.fillAmount, m_newFillAmount, m_reduceHealhAnimationSpeed * Time.deltaTime);
     }
 
     public void TakeDamage(float damage)
@@ -36,6 +40,6 @@ public class HealthSystem : MonoBehaviour
 
     public void UpdateHealthBar()
     {
-        m_healthBarSpriteForeground.fillAmount = m_currentHealth / m_maxHealth;
+        m_newFillAmount = m_currentHealth / m_maxHealth;
     }
 }
